42679c85222eae3c395bc6365c472ec5058f6c24
JSPRO-11(Subham Saha) <js-pro-11@JSPRO-11s-MacBook-Pro.local>
2022-12-26 13:41:34 +0530

    media completed

--- pages/media/list.vue
+++ pages/media/list.vue
@@ -1,64 +1,38 @@
 <template>
     <div>
-        <BreadcrumbComponent main-page="Subscription" current-page="List" />
+        <BreadcrumbComponent main-page="Media" current-page="List" />
         <section class="content">
             <div class="row">
 
                 <div class="col-12">
                     <div class="box">
                         <div class="box-header d-flex justify-content-between align-items-center">
-                            <h4 class="box-title">Subscription</h4>
-                            <NuxtLink to="/subscription/create"><el-button type="warning">Create</el-button></NuxtLink>
+                            <h4 class="box-title">Media</h4>
+                            <div>
+                                <NuxtLink to="/media/create"><el-button type="warning">Create</el-button></NuxtLink>
+                            </div>
                         </div>
                         <div class="box-body">
                             <el-table :data="tableData" style="width: 100%" max-height="100%">
-                                <el-table-column fixed prop="id" label="ID" width="150">
+                                <el-table-column fixed prop="id" label="ID">
                                 </el-table-column>
-                                <el-table-column prop="name" label="Name" width="250">
-                                </el-table-column>
-                                <el-table-column prop="email" label="Email" width="250">
-                                </el-table-column>
-                                <el-table-column prop="phone" label="Phone" width="250">
-                                </el-table-column>
-                                <el-table-column label="E-Book" width="250">
+                                <el-table-column label="Type" width="250">
                                     <template slot-scope="scope">
-                                        <el-tag v-if="scope.row.ebook" size="medium" type="success">Interested</el-tag>
-                                        <el-tag v-else size="medium" type="danger">Not Interested</el-tag>
+                                        <el-tag v-if="scope.row.type===1" size="medium" type="success">Image</el-tag>
+                                        <el-tag v-else size="medium" type="danger">Video</el-tag>
                                     </template>
                                 </el-table-column>
-                                <el-table-column label="Event" width="250">
-                                    <template slot-scope="scope">
-                                        <el-tag v-if="scope.row.event" size="medium" type="success">Interested</el-tag>
-                                        <el-tag v-else size="medium" type="danger">Not Interested</el-tag>
-                                    </template>
+                                <el-table-column prop="media" label="Media">
                                 </el-table-column>
-                                <el-table-column label="Blog" width="250">
-                                    <template slot-scope="scope">
-                                        <el-tag v-if="scope.row.blog" size="medium" type="success">Interested</el-tag>
-                                        <el-tag v-else size="medium" type="danger">Not Interested</el-tag>
-                                    </template>
-                                </el-table-column>
-                                <el-table-column label="Newsletter" width="250">
-                                    <template slot-scope="scope">
-                                        <el-tag v-if="scope.row.newsletter" size="medium" type="success">Interested</el-tag>
-                                        <el-tag v-else size="medium" type="danger">Not Interested</el-tag>
-                                    </template>
-                                </el-table-column>
-                                <el-table-column label="Crossword" width="250">
-                                    <template slot-scope="scope">
-                                        <el-tag v-if="scope.row.crossword" size="medium" type="success">Interested</el-tag>
-                                        <el-tag v-else size="medium" type="danger">Not Interested</el-tag>
-                                    </template>
-                                </el-table-column>
-                                <el-table-column label="CreatedAt" width="250">
+                                <el-table-column label="CreatedAt">
                                     <template slot-scope="scope">
                                         {{$dateFns.format(new Date(scope.row.created_at), 'dd-MMM-yyyy hh:mm aa')}}
                                     </template>
                                 </el-table-column>
-                                <el-table-column fixed="right" label="Operations" width="200">
+                                <el-table-column label="Operations" width="200">
                                     <template slot-scope="scope">
-                                        <NuxtLink :to="`/subscription/edit/${scope.row.id}`"><el-button type="primary" icon="el-icon-edit" circle></el-button></NuxtLink>
-                                        <NuxtLink :to="`/subscription/display/${scope.row.id}`"><el-button type="info" icon="el-icon-view" circle></el-button></NuxtLink>
+                                        <NuxtLink :to="`/media/edit/${scope.row.id}`"><el-button type="primary" icon="el-icon-edit" circle></el-button></NuxtLink>
+                                        <NuxtLink :to="`/media/display/${scope.row.id}`"><el-button type="info" icon="el-icon-view" circle></el-button></NuxtLink>
                                         <el-popconfirm
                                         confirm-button-text='OK'
                                         cancel-button-text='No, Thanks'
@@ -120,7 +94,7 @@ export default {
                 fullscreen: true,
             });
             try {
-                const response = await this.$privateApi.get('/api/subscription/paginate?page='+page); // eslint-disable-line
+                const response = await this.$privateApi.get('/api/media/paginate?page='+page); // eslint-disable-line
                 this.tableData = response?.data?.data?.data
                 this.count = response?.data?.data?.total
                 this.currentPage = this.$route.query.page ? Number(this.$route.query.page) : 1;
@@ -140,7 +114,7 @@ export default {
             });
             try {
                 // eslint-disable-next-line no-unused-vars
-                const response = await this.$privateApi.delete('/api/subscription/delete/'+id);
+                const response = await this.$privateApi.delete('/api/media/delete/'+id);
                 const newTableData = this.tableData.filter((item)=>{
                     return item.id!==id;
                 })
