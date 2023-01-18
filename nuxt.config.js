22356932b16306771b0f68d27341e47730eabf0d
JSPRO-11(Subham Saha) <js-pro-11@JSPRO-11s-MacBook-Pro.local>
2023-01-18 16:20:23 +0530

    changed the admin issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 
 export default {
+  server: {
+    port: 3300, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Hrudayaspandana',
